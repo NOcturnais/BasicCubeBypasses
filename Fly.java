9fa85ef6a6e94ece44fbc0b2381c3c5d3b3ca663
NOctu <32684737+NOcturnais@users.noreply.github.com>
2020-05-28 19:22:53 +0200

    Added a description

--- Fly.java
+++ Fly.java
@@ -21,3 +21,13 @@ public void onUpdate(EventUpdate e){
 		MovementUtils.setSpeed(0);  //Stop all player movements
 	}
 }
+
+/*So now we are going to speak of how this very basic fly bypass Sentinel uwu
+  First we make the player take a damage to disable the fly check (I discovered this randomly lmao)
+  Next we says to the server that the player isn't on ground and put the motion to 0 to make the player float in the air.
+  If the player go down, sentinel will detect it
+  The slow timer is the base for every cubecraft movements hacks (noob)
+  Next we say to the game that every 2 ticks he is going to move the player forward of 2 blocks
+  And finally when we disable the hack we put a normal timer and set the motion Y to -1 to make the player fall fast and set the the speed to 0 to stop all movements of the player
+  Sorry for my bad english boys x)
+*/
