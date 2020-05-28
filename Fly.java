267c1d89243f0e1020a51378f4da3bfc412fefbc
NOctu <32684737+NOcturnais@users.noreply.github.com>
2020-05-28 19:09:55 +0200

    Added a basic fly

---
+++ Fly.java
@@ -0,0 +1,24 @@
+@Override
+public void onEnable(){
+  super.onEnable();
+  MovementUtils.Damage(); //Take a damage
+}
+@Override
+public void onDisable(){
+  super.onDisable();
+  mc.timer.timerSpeed = 1f; //Set a normal timer
+  mc.thePlayer.motionY = -1f; //Make the player fall fast to bypass the fly check
+  MovementUtils.setSpeed(0); //Stop all player movements
+}
+@EventTarget
+public void onUpdate(EventUpdate e){
+  mc.thePlayer.onGround = false;
+  mc.thePlayer.motionY = 0; //Make the player stay in the air
+	if(mc.thePlayer.ticksExisted % (2 * 1)== 0) { //Every 2 ticks
+    mc.timer.timerSpeed = 0.6f; //Put a slow timer
+		MovementUtils.setSpeed(2f); //Set the player speed to 2f
+	}else {
+		mc.timer.timerSpeed = 0.1f; //Put a VERY slow timer
+		MovementUtils.setSpeed(0);  //Stop all player movements
+	}
+}
